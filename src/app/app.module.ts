e9345ff128170952f724b9f4ac3f87f85d11a978
Travis Li <coolki@gmail.com>
2017-09-09 23:14:24 +0800

    add activatePageModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {ErrorHandler, LOCALE_ID, NgModule} from '@angular/core';
+import { ErrorHandler, LOCALE_ID, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
@@ -8,13 +8,14 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import {MsgBoxModule} from "../components/msg-box/msg-box.module";
-import {LoginPageModule} from "../pages/login/login.module";
-import {AuthService} from "../providers/auth/auth.service";
-import {HttpModule} from "@angular/http";
-import {LessonService} from "../providers/lesson.service";
-import {TimetableTabPageModule} from "../pages/timetable-tab/timetable-tab.module";
-import {CachedLessonService} from "../providers/cached-lesson.service";
+import { MsgBoxModule } from "../components/msg-box/msg-box.module";
+import { LoginPageModule } from "../pages/login/login.module";
+import { ActivatePageModule } from "../pages/activate/activate.module";
+import { AuthService } from "../providers/auth/auth.service";
+import { HttpModule } from "@angular/http";
+import { LessonService } from "../providers/lesson.service";
+import { TimetableTabPageModule } from "../pages/timetable-tab/timetable-tab.module";
+import { CachedLessonService } from "../providers/cached-lesson.service";
 
 
 @NgModule({
@@ -26,9 +27,10 @@ import {CachedLessonService} from "../providers/cached-lesson.service";
   imports: [
     BrowserModule,
     HttpModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp,{backButtonText:'',}),
     MsgBoxModule,
     LoginPageModule,
+    ActivatePageModule,
     TimetableTabPageModule
   ],
   bootstrap: [IonicApp],
