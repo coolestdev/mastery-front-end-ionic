3664f761313ca6c626a3e41fda6d9b69183d0336
TravisLi <coolki@gmail.com>
2017-09-12 16:12:09 +0800

    remove the use of msg box and use loading ctrl

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import {LoginPage} from "./login";
-import {MsgBoxModule} from "../../components/msg-box/msg-box.module";
 
 @NgModule({
   declarations: [
@@ -9,7 +8,6 @@ import {MsgBoxModule} from "../../components/msg-box/msg-box.module";
   ],
   imports: [
     IonicPageModule.forChild(LoginPage),
-    MsgBoxModule
   ],
   exports: [
     LoginPage
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,7 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Logger} from "../../providers/logger/logger";
 import {AuthService} from "../../providers/auth/auth.service";
-import {Alert, AlertController, IonicPage, NavController} from "ionic-angular";
-import {MsgBoxComponent} from "../../components/msg-box/msg-box.component";
+import {AlertController, LoadingController, ToastController, IonicPage, NavController} from "ionic-angular";
 
 @IonicPage({
   name: 'login'
@@ -15,9 +14,6 @@ export class LoginPage {
 
   private logger:Logger = Logger.getLogger(this.constructor.name);
 
-  @ViewChild(MsgBoxComponent)
-  msgBox:MsgBoxComponent;
-
   message: string;
   waiting: boolean;
   @Input() username: string;
@@ -27,6 +23,8 @@ export class LoginPage {
     public authService: AuthService,
     public nav: NavController,
     public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController,
+    public toastCtrl: ToastController,
   ) {
     this.waiting = false;
     this.username = '';
@@ -34,7 +32,7 @@ export class LoginPage {
   }
 
   ngOnInit(){
-    this.msgBox.clearMsg();
+
     if (localStorage.getItem('username') != null) this.username=localStorage.getItem('username');
     this.password='';
   }
@@ -59,6 +57,14 @@ export class LoginPage {
     alert.present();
   }
 
+  showLoading(content: string): any {
+    let loading = this.loadingCtrl.create({
+      content: content,
+      cssClass: 'customLoading'
+    });
+    return loading;
+  }
+
   autoLogin() {
     this.username='';
     this.password='';
@@ -74,11 +80,16 @@ export class LoginPage {
   }
 
   login() {
-    this.msgBox.sendPriMsg('登入中...');
+
     this.waiting = true;
+
+    let loading = this.showLoading('登入中...');
+    loading.present();
+
     this.authService.login(this.username, this.password)
       .then(
         (value) => {
+          loading.dismiss();
           console.log(value);
           localStorage.setItem('username', this.username);
           this.waiting = false;
@@ -87,18 +98,28 @@ export class LoginPage {
             this.logger.debug(redirect);
             this.nav.setRoot(redirect);
           }else{
-            this.msgBox.sendWarningMsg('密碼錯誤');
+            this.showMsg('密碼錯誤');
           }
         }
       )
       .catch((reject)=>{
           this.waiting = false;
           this.logger.error(reject);
-          this.msgBox.sendAlterMsg('無此用戶');
+          this.showMsg('無此用戶');
         }
 
       )
   }
+
+  private showMsg(msg:string){
+    let toast = this.toastCtrl.create({
+      message: `${msg}`,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
+  }
+
 
   clearInput() {
     this.username = '';
