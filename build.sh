085b146a1edceef5cb4b803bfeb36629a9bfcff5
thcathy <thcathy@gmail.com>
2018-04-18 22:27:52 +0800

    add simple build script

---
+++ build.sh
@@ -0,0 +1,25 @@
+set -e
+
+buildWebProd() {
+  ionic cordova build browser --prod
+}
+
+buildios() {
+ ionic cordova build ios --prod 
+}
+
+simios() {
+ ionic cordova emulate ios -lc
+}
+
+deployWeb() {
+  firebase deploy
+}
+
+buildWebAndDeploy() {
+  buildWebProd;
+  deployWeb;
+}
+
+"$@"
+
